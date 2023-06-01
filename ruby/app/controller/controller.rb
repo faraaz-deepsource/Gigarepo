2893ba3cde354daa036c5995b838d0a6dd86bfe1
Syed Faraaz Ahmad <113512303+faraaz-deepsource@users.noreply.github.com>
2023-06-01 16:50:53 +0530

    Update controller.rb
    Signed-off-by: Syed Faraaz Ahmad <113512303+faraaz-deepsource@users.noreply.github.com>

--- ruby/app/controller/controller.rb
+++ ruby/app/controller/controller.rb
@@ -1,3 +1,18 @@
-class SomeController < Rails::ActionController
+class SomeController <      Rails::ActionController
   
+  
+  
+  
+  def index; end
+  
+  def show
+        User.all
+  Comments.all.first
+    500.times do 
+          pp 1_000
+    end
+    
+    
+    
+    
 end
